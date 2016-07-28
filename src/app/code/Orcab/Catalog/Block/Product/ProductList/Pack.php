3d492373db20550b90a47036cb6381464b816b3e
Matthieu Prigent <matthieu.prigent@smile.fr>
2016-07-28 14:47:47 +0200

    Product associations blocks on FO

---
+++ src/app/code/Orcab/Catalog/Block/Product/ProductList/Pack.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Pack products block
+ *
+ * @category  Orcab
+ * @author    Matthieu Prigent <mapri@smile.fr>
+ * @copyright 2016 Orcab
+ */
+namespace Orcab\Catalog\Block\Product\ProductList;
+
+use \Magento\Catalog\Block\Product\ProductList\Upsell;
+
+class Pack extends Upsell
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function _prepareData()
+    {
+        $product = $this->_coreRegistry->registry('product');
+        /* @var $product \Orcab\Catalog\Rewrite\Model\Product */
+        $this->_itemCollection = $product->getPackProductCollection()->setPositionOrder()->addStoreFilter();
+        if ($this->moduleManager->isEnabled('Magento_Checkout')) {
+            $this->_addProductAttributesAndPrices($this->_itemCollection);
+        }
+        $this->_itemCollection->setVisibility($this->_catalogProductVisibility->getVisibleInCatalogIds());
+
+        $this->_itemCollection->load();
+
+        /**
+         * Updating collection with desired items
+         */
+        $this->_eventManager->dispatch(
+            'catalog_product_substitute',
+            ['product' => $product, 'collection' => $this->_itemCollection, 'limit' => null]
+        );
+
+        foreach ($this->_itemCollection as $product) {
+            $product->setDoNotUseCategoryId(true);
+        }
+
+        return $this;
+    }
+}
